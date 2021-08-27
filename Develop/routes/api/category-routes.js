076f37e83ba6a73dff06be3da76d3afb53e96f05
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-08-27 14:48:27 -0400

    routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,74 +4,71 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 // find all categories
 // be sure to include its associated Products
-router.get('/', async (req, res) => {
-  try {
-    const categoryData = await Category.findAll({ includes: Product });
-    res.status(200).json(categoryData);
-
-  } catch (err) {
-    res.status(500).json(err);
-  };
+router.get('/', (req, res) => {
+  Category.findAll({
+    include: {
+      model: [Product],
+    },
+  })
+    .then((categoryData) => { res.json(categoryData) })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // find one category by its `id` value
 // be sure to include its associated Products
 router.get('/:id', async (req, res) => {
-  try {
-    const categoryData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product }]
-    })
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(404).json({ message: "No category found with this matching ID." });
-  };
+  Category.findOne({
+    include: {
+      model: [Product],
+    },
+  })
+    .then((categoryData) => { res.json(categoryData) })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// create a new category
 router.post('/', (req, res) => {
-  // create a new category
-  try {
-    const newCategory = await Category.create(req.body);
-    res.status(200).json(newCategory);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+  Category.create(req.body)
+    .then((categoryData) => { res.json(categoryData) })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-  try {
-    const categoryData = await Category.update(req.body, {
-      where: {
-        id: req.params.id,
-      }
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((categoryData) => { res.json(categoryData) })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-    if (!categoryData) {
-      res.status(404).json({ message: "No category found with this matching ID" })
-      return
-    }
-    res.status(200).json(categoryData)
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-  try {
-    const categoryData = await Category.destroy({
-      where: {
-        id: req.params.id
-      }
-    });
 
-    if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this matching ID.' });
-      return;
-    }
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+// delete a category by its `id` value
+router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((categoryData) => { res.json(categoryData) })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -2,37 +2,60 @@ const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
-
-// get all products
+// get all products 
+// find all products
+// be sure to include its associated Category and Tag data
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      Category, {
+        model: Tag,
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((products) => res.json(products))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
-try {
-  const productInfo = await.Product.findByPk({ include: Category, Tag });
-  res.status(200).json(productInfo);
-} catch (err) {
-  res.status(404).json({ message: "No product foind by that Category/Tag combo" });
-};
 
+// get one product  
+// find a single product by its `id`
+// be sure to include its associated Category and Tag data
 
-// get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((products) => res.json(products))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 // create new product
+/* req.body should look like this...
+   {
+     product_name: "Basketball",
+     price: 200.00,
+     stock: 3,
+     tagIds: [1, 2, 3, 4]
+   }
+ */
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -97,8 +120,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
+// delete one product by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(productData => {
+      if (!productData) {
+        res.status(404).json({ message: 'No product with that ID' });
+        return;
+      }
+      res.json(productData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
